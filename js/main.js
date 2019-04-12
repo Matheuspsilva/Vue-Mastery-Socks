302d7e5e7c8f12d3356e2489c64a5fddb2d0cb36
Matheuspsilva <matheuspsilva29@gmail.com>
2019-04-12 10:59:33 -0300

    aula 7

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
+Vue.component('product',{
+    props: [message],
+    template: `
+        <div>
+        {{message}}}
+        </div>
+`
+})
+
+
 var app = new Vue({
     el:'#app',
     data:{
+        brand: 'Vue Mastery',
         product:"Socks",
-        image:"assets/vmSocks-green-onWhite.jpg",
+        selectedVariant:0,
         link: 'https://www.amazon.com/s?k=socks&ref=nb_sb_noss',
-        inventory:0,
-        inStock: false,
         sale: false,
+        inventory:0,
         sizes:['S','M','L','XL'],
         details: ["80% cotton","20% polyester", "Gender-neutral"],
         variants:[
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage:"assets/vmSocks-green-onWhite.jpg"
+                variantImage:"assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId:2235,
                 variantColor: "blue",
-                variantImage:"assets/vmSocks-blue-onWhite.jpg"
+                variantImage:"assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
 
             }
         ],
@@ -34,10 +46,24 @@ var app = new Vue({
             }
 
         },
-        updateProduct:function(variantImage){
-            this.image = variantImage
+        updateProduct:function(index){
+            this.selectedVariant = index
         }
+    },
+    
+    computed:{
+        title(){
+            return this.brand +' '+ this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+        
     }
 })
+
         
         
